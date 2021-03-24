1ec2237804474a3ca75972bb6e38bfa8bb5f11f2
Vashkevich Ekaterina <veo_25@mail.ru>
2021-03-24 12:56:00 +0300

    Fix style and logic of code

--- main.rb
+++ main.rb
@@ -1,38 +1,38 @@
-require "rubygems"
-require "minitest/autorun"
+# frozen_string_literal: true
 
+require 'rubygems'
+require 'minitest/autorun'
+
+# piglatin language translator
 class PiglatinTranslatorTest < MiniTest::Test
-    VOWELS = ['a','e','i','o','u']
+  VOWELS = %w[a e i o u].freeze
 
-    def test_pig_becomes_igpay
-        assert_equal "igpay", translate("pig")
-    end
+  def test_pig_becomes_igpay
+    assert_equal 'igpay', translate('pig')
+  end
 
-    def test_eat_becomes_eatyay
-        assert_equal "eatyay", translate("eat")
-    end
+  def test_eat_becomes_eatyay
+    assert_equal 'eatyay', translate('eat')
+  end
 
-    def test_string_becomes_ingstray
-        assert_equal "ingstray", translate("string")
-    end
+  def test_string_becomes_ingstray
+    assert_equal 'ingstray', translate('string')
+  end
 
-    def test_will_smile_egg_becomes_illway_ilesmay_eggyay
-        assert_equal "illway ilesmay eggyay", translate_sentence("will smile egg")
-    end
+  def test_will_smile_egg_becomes_illway_ilesmay_eggyay
+    assert_equal 'illway ilesmay eggyay', translate('will smile egg')
+  end
 
-    def translate_sentence(string)
-        words = string.split(" ")
-        new_words = words.map {|word| translate(word)}
-        new_words.join(" ")
+  def translate(string)
+    words = string.split(' ')
+    new_words = words.map do |word|
+      if VOWELS.include? word[0]
+        "#{word}yay"
+      else
+        index_vowel = word.chars.index { |x| VOWELS.include? x }
+        "#{word[index_vowel..-1]}#{word[0..index_vowel - 1]}ay"
+      end
     end
-
-    private
-    def translate(word)
-        if VOWELS.include? word[0]
-            word + "yay"
-        else
-            index_vowel = word.chars.index {|x| VOWELS.include? x}
-            word[index_vowel..-1]+ word[0..index_vowel-1]+"ay"
-        end 
-    end
+    new_words.join(' ')
+  end
 end
